641144b5afab869f6f5810e086e01760bf094d15
tthanhthien <tthanhthien@savills.com.vn>
2024-03-23 10:55:22 +0700

    color picker

--- src/app/modules/web-tool/color-picker/color-picker.component.ts
+++ src/app/modules/web-tool/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ColorPickerModule} from "primeng/colorpicker";
 import {FormsModule} from "@angular/forms";
 import {ColorPaletteComponent} from "./color-palette/color-palette.component";
@@ -19,18 +19,45 @@ import {NgStyle} from "@angular/common";
   styleUrl: './color-picker.component.scss'
 })
 export class ColorPickerComponent {
-  public hue = ''
-  public color = '';
-  hexColor: any;
+  hue = '';
+  rgbValue = '';
+  hexValue = '';
+  cmykValue = '';
 
-  convertHexToRgb(hexColor: string) {
-    if (!hexColor) {
-      return '';
-    }
-    const r = parseInt(hexColor.slice(1, 3), 16);
-    const g = parseInt(hexColor.slice(3, 5), 16);
-    const b = parseInt(hexColor.slice(5, 7), 16);
+  onSelectColor(rgbValue: string[]) {
+    this.rgbValue = rgbValue.join(', ');
+    this.hexValue = this.rgbToHex(rgbValue);
+    this.cmykValue = this.rbgToCmyk(rgbValue).map(item => `${Math.round(item * 100)}%`).join(', ');
+  }
 
-    return `${r}, ${g}, ${b}`;
+  rgbToHex(rgb: any[]) {
+    const convertToHex = (c: any) => {
+      const hex = parseInt(c).toString(16);
+      return hex.length == 1 ? '0' + hex : hex;
+    };
+    return '#' + convertToHex(rgb[0]) + convertToHex(rgb[1]) + convertToHex(rgb[2]);
+  }
+
+  rbgToCmyk(rgb: any[]) {
+    let computedC = 0;
+    let computedM = 0;
+    let computedY = 0;
+    let computedK = 0;
+
+    let r = parseInt(rgb[0]);
+    let g = parseInt(rgb[1]);
+    let b = parseInt(rgb[2]);
+
+    computedC = 1 - (r / 255);
+    computedM = 1 - (g / 255);
+    computedY = 1 - (b / 255);
+
+    const minCMY = Math.min(computedC, Math.min(computedM, computedY));
+    computedC = (computedC - minCMY) / (1 - minCMY);
+    computedM = (computedM - minCMY) / (1 - minCMY);
+    computedY = (computedY - minCMY) / (1 - minCMY);
+    computedK = minCMY;
+
+    return [computedC, computedM, computedY, computedK];
   }
 }

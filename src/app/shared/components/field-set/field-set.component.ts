3003ed41e08a44dda941e381ff1796413bbbd4a7
tthanhthien <tthanhthien@savills.com.vn>
2024-05-01 15:05:18 +0700

    tool box

---
+++ src/app/shared/components/field-set/field-set.component.ts
@@ -0,0 +1,16 @@
+import {Component, ContentChild, ElementRef, Input, TemplateRef} from '@angular/core';
+import {NgTemplateOutlet} from "@angular/common";
+
+@Component({
+  selector: 'app-field-set',
+  standalone: true,
+  imports: [
+    NgTemplateOutlet
+  ],
+  templateUrl: './field-set.component.html',
+  styleUrl: './field-set.component.scss'
+})
+export class FieldSetComponent {
+  @Input() legend = '';
+  @ContentChild('content') content!: TemplateRef<any>;
+}
